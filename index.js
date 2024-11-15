6bacee80bc6821002501003359da8eb0a20118f7
Rohitsharma7877 <rohitsharma.sharma194@gmail.com>
2024-11-15 17:15:11 +0530

    add

--- index.js
+++ index.js
@@ -4,31 +4,35 @@ const axios = require("axios");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: "*", // You can also specify specific domains if needed
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
-// API key from environment variable
-const API_KEY = process.env.NEWS_API_KEY || "c003f70919aa4fc4bb23071b5c77755e";
+const API_KEY = process.env.API_KEY;
 
 app.get("/", (req, res) => {
   res.send("Welcome to NEWS App");
 });
 
-// News endpoint
 app.get("/news", async (req, res) => {
-    console.log(res,"ressss")
-  const { country = "us", category } = req.query; // Get optional query params
+  console.log(res, "ressss");
+  const { country = "us", category } = req.query;
+
   let apiUrl = `https://newsapi.org/v2/top-headlines/sources?country=${country}&apiKey=${API_KEY}`;
-  
+
   if (category) {
     apiUrl = `https://newsapi.org/v2/top-headlines/sources?category=${category}&apiKey=${API_KEY}`;
   }
-
   try {
     const response = await axios.get(apiUrl);
-    res.json(response.data); // Return the fetched data
+    res.json(response.data);
   } catch (err) {
-    console.error(err.message);
+    console.error(
+      "Error fetching news:",
+      err.response ? err.response.data : err.message
+    );
     res.status(500).json({ error: "Failed to fetch news data" });
   }
 });
